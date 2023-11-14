f95343759ea253bdb8abb562a905664451275222
alelade <kyazgan@gmail.com>
2023-11-14 06:10:20 +0300

    error mitigated

--- Book.Business/BookManagement.cs
+++ Book.Business/BookManagement.cs
@@ -14,10 +14,6 @@ public class BookManagement
     {
         return BusinessResult<bool>.Ok(true);
     }
-    public BusinessResult<IEnumerable<global::Book>> Get()
-    {
-        return BusinessResult<IEnumerable<global::Book>>.Ok(new List<Book>());
-    }
     public BusinessResult<bool> Update()
     {
         return BusinessResult<bool>.Ok(true);
